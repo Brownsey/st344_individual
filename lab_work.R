d3c68a0682d30ac6e450a58a2dd3e097213d6c97
Brownsey <brownsey666@gmail.com>
2020-01-04 23:26:09 +0000

    look at part 1

--- lab_work.R
+++ lab_work.R
@@ -3,6 +3,9 @@ library(tidyverse)
 library(lubridate)
 #Apparently some saucy palettes in here :O for scale continuous
 library(viridis) 
+#Supporting information
+#https://digital.nhs.uk/data-and-information/publications/statistical/appointments-in-general-practice/appointments-in-general-practice-supporting-information
+
 file_loc <- "C:/Users/Stephen/Desktop/University Work/Year 3 uni/St344/"
 monthYears <- paste0(month.abb[c(3:12,1:8)], "_", c(rep(18,10),rep(19,8)))
 d <- list()
@@ -25,6 +28,9 @@ covData <- tibble()
 for (i in 1:length(monthYears)) {
   covData <- rbind(covData, filter(d[[i]], CCG_NAME=="NHS Coventry and Rugby CCG"))
 }
+#This code removed the d variable from the environment and should speed things up
+#As the d variable is over 500mb and causes performance issues in rstudio
+#remove(d)
 
 
 covData <- select(covData, Appointment_Date, APPT_STATUS, HCP_TYPE, APPT_MODE, TIME_BETWEEN_BOOK_AND_APPT, CCG_CODE,
@@ -62,8 +68,9 @@ covData %>%
 
 #importing next dataset which involves which patients are registered at each practice
 app_gp_coverage <- import(paste0(file_loc, "Appointments_GP_Daily_Aug19/APPOINTMENTS_GP_COVERAGE.csv"), setclass = "tibble")%>%
-  mutate(Appointment_Month = parse_date_time(Appointment_Month, "%d-%b-%Y")) %>%
-  mutate(month_year = parse_date_time(Appointment_Month, "%Y-%m"))
+  mutate(Appointment_Month = parse_date_time(Appointment_Month, "%d-%b-%Y"))
+#%>%
+#  mutate(month_year = parse_date_time(Appointment_Month, "%Y-%m"))
 
 for (i in 1:length(colnames(app_gp_coverage))) {
   colnames(app_gp_coverage)[i] = tolower(colnames(app_gp_coverage)[i])
@@ -77,7 +84,7 @@ data <- inner_join(covData, app_gp_coverage, by = c("ccg_code" = "commissioner_o
   filter(year(appointment_date) == year(appointment_month) &
            month(appointment_date) == month(appointment_month))
 
-##Copy pasted code from the modelling section of the lab
+##Copy pasted code from the modelling section of the lab, not sure if this is actually correct - but anyway
 
 poisson_data <- data %>%
   group_by(appointment_date) %>%
@@ -97,3 +104,86 @@ my_model <- glm(count ~ 0 + weekday + offset(log_registered_patients), family="p
 #Not really sure about this at all lol
 my_model$coefficients[2]/my_model$coefficients[6]
 
+
+###The first question was provided by your client and explores if the demand on Coventry GPs has increased over the recent past.
+###As there are various ways of measuring the demand on GPs across different appointment types, you will need to refine the question
+###to make it sufficiently specific and answerable by using the data.
+
+#The first step of this problem is to undestand how each appointment type is handled by a GP.
+#We have the following options: Face to Face, Home Visit, Telephone, unknown and Video conference.
+#Video conference is so few in number that they can be discarded (23 over the entire time period)
+#Step 1, graph the data to see general trend over time.
+#Graph datapoints by week to take account of different
+#Colour the graph potentially by season and also by  type of appointment
+#Getting data by week:
+data <- data %>%
+  #Detecting week number of appointment date
+  mutate(week_num = isoweek(appointment_date)) %>%
+  mutate(year = year(appointment_date)) %>%
+  mutate(year = if_else(year == 2018, 0, 1)) %>%
+  mutate(week_num = week_num + (52 * year)) %>%
+  #Taking account of the fact that 1 actually refers to week 53
+  mutate(week_num = if_else(week_num == 1, 53, week_num)) %>%
+  mutate(week_num = week_num - 8)
+
+
+#Count summaries of the data by
+appt_by_mode <- data %>% 
+  group_by(appt_mode, week_num) %>%
+  summarise(count = sum(count_of_appointments)) %>%
+  ungroup()
+
+#General plot for understanding
+appt_by_mode %>%
+  ggplot() +
+  geom_point(aes(x = week_num, y = count, colour = appt_mode)) +
+  theme_bw()
+
+#Looking into the two low results for Face-to-Face
+appt_by_mode %>% 
+  filter(count < 20000 & appt_mode == "Face-to-Face")
+
+#Returns week 9 (first week and not a full one, only 4 results) and 52 (over Christmas so less visits)
+data %>%
+  filter(week_num %in% c(9, 52)) %>%
+  select(appointment_date) %>%
+  unique()
+
+#No appointments on 30th December
+data %>% 
+  filter(str_detect(appointment_date, '12-30'))
+
+
+##Therefore we remove these two weeks (9 and 52).
+data <- data %>%
+  filter(!week_num %in% c(9, 52))
+
+appt_by_mode <- data %>% 
+  group_by(appt_mode, week_num) %>%
+  summarise(count = sum(count_of_appointments)) %>%
+  ungroup()
+
+appt_by_mode %>%
+  ggplot() +
+  geom_point(aes(x = week_num, y = count, colour = appt_mode)) +
+  theme_bw()
+
+appt_by_mode %>%
+  filter(appt_mode == "Face-to-Face") %>%
+  ggplot(aes(x = week_num, y = count)) +
+  geom_point()
+
+appt_by_mode %>%
+  filter(appt_mode == "Home Visit") %>%
+  ggplot(aes(x = week_num, y = count)) +
+  geom_point()
+
+appt_by_mode %>%
+  filter(appt_mode == "Telephone") %>%
+  ggplot(aes(x = week_num, y = count)) +
+  geom_point()
+
+appt_by_mode %>%
+  filter(appt_mode == "Unknown") %>%
+  ggplot(aes(x = week_num, y = count)) +wwwwwww
+  geom_point()
